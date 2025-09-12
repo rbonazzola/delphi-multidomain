50b1069ee8c94f1c72537b2bb56c06d5cfa6638f
rbonazzola <rodbonazzola@gmail.com>
2025-09-12 08:47:47 +0100

    Bash script for training on drug prescription data

--- train_drugs.sh
+++ train_drugs.sh
@@ -1,10 +1,12 @@
-TEST_FOLD=1
+TEST_FOLD=${TEST_FOLD:=1}
+MAX_STEPS=${MAX_STEPS:=1000}
+EXPERIMENT_NAME="test-drugs"
 
 python train.py \
   config/train_delphi-drugs.py \
   --data data/transforms/drugs/all.bin \
   --test_fold $TEST_FOLD \
-  --experiment-name testcito \
-  --max_steps=100 \
+  --experiment-name ${EXPERIMENT_NAME} \
+  --max_steps=${MAX_STEPS} \
   --eval_iters=100 \
   "$@"
